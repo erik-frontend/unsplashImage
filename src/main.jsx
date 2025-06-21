47c9815baf5d2a965ecec0b4773a513b75afe417
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-06-21 10:03:39 +0300

    commit
    add context AppProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { AppProvider } from './components/context.js'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <QueryClientProvider>
-    <App />
-    <ReactQueryDevtools initialIsOpen = {false}/>
-  </QueryClientProvider>,
+  <AppProvider>
+    <QueryClientProvider>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>,
+  </AppProvider>
 )
