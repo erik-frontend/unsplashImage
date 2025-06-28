0f9786ede53f0ea7392aa89a0c2eed0b18bf93a6
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-06-28 10:09:15 +0300

    commit

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -1,8 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import React from 'react'
+import { useGlobalContext } from './context'
+const url = "https://api.unsplash.com/search/photos/?client_id=O87v4AmV7BrvdapXw77SUcHSkdrjFw7b-_q8GMfl8fU&query=cat"
 
 const Galery = () => {
+
+  const { serchItme } = useGlobalContext()
+
+  const response = useQuery({
+    queryKey: ["images", serchItme],
+    queryFn: async () => {
+      const result = await axios.get(`${url}&query=${serchItme}`)
+      return result.data
+    }
+  })
+  console.log(response);
+  
   return (
-    <div>Galery</div>
+    <></>
   )
 }
 
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useGlobalContext } from './context'
+
 
 const SearchForm = () => {
+
+  const {setSerchItme} = useGlobalContext()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const searchValue = e.target.elements.search.value
+    // console.log(searchValue);
+
+    if(!searchValue) return
+    setSerchItme(searchValue)
+    
+  }
+
   return (
-    <div>SearchForm</div>
+    <section>
+      <h1 className="title">Unsplash Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder='cat' name='search' className="form-input search-input" />
+        <button className="btn" type='submit'>Search</button>
+      </form>
+    </section>
   )
 }
 
