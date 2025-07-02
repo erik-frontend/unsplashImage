b364877fd8b787fddbe0479867ca1e3301821a99
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-07-02 16:01:08 +0300

    commit

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import React from 'react'
 import { useGlobalContext } from './context'
-const url = "https://api.unsplash.com/search/photos/?client_id=O87v4AmV7BrvdapXw77SUcHSkdrjFw7b-_q8GMfl8fU&query=cat"
+const url = "https://api.unsplash.com/search/photos/?per_page=20&client_id=O87v4AmV7BrvdapXw77SUcHSkdrjFw7b-_q8GMfl8fU"
 
 const Galery = () => {
 
@@ -16,9 +16,34 @@ const Galery = () => {
     }
   })
   console.log(response);
-  
+  if (response.isLoading) {
+    return (
+      <section className='image-container'><h4>Loading...</h4></section>
+    )
+  }
+  if (response.isError) {
+    return (
+      <section className='image-container'><h4>There are an Error...</h4></section>
+    )
+  }
+
+  const results = response.data.results
+console.log(results);
+
+  if (results.length < 1) {
+    return (
+      <section className='image-container'><h4>No results found...</h4></section>
+    )
+  }
+
   return (
-    <></>
+    <section className='image-container'>
+      {results.map(item => {
+        const url = item.urls.regular
+        return <img src={url} alt={item.alt_description} key={item.id} className='img'/>
+      }
+      )}
+    </section>
   )
 }
 
--- src/components/context.jsx
+++ src/components/context.jsx
@@ -2,21 +2,31 @@ import { useContext, createContext, useState, useEffect } from "react";
 
 const AppContext = createContext()
 
-export const AppProvider = ({children}) => {
-    const [isDarkTheme, setIsDarkTheme] = useState(false)
-    const [serchItme, setSerchItme] = useState("wallpapers")
+export const AppProvider = ({ children }) => {
+    const getTheme = () => {
+        return localStorage.getItem("darkTheme") === "true"
+    }
+
+    const [isDarkTheme, setIsDarkTheme] = useState(getTheme())
+    const [serchItme, setSerchItme] = useState("ocean")
 
     const toggleDarkTheme = () => {
         const newDarkTheme = !isDarkTheme
         setIsDarkTheme(newDarkTheme)
+        localStorage.setItem("darkTheme", newDarkTheme)
         document.body.classList.toggle("dark-theme", newDarkTheme)
     }
 
+    useEffect(() => {
+        document.body.classList.toggle("dark-theme", isDarkTheme)
+    }, [isDarkTheme])
+
+
     return (
         <AppContext.Provider value={{
-            isDarkTheme,toggleDarkTheme,setSerchItme,serchItme
+            isDarkTheme, toggleDarkTheme, setSerchItme, serchItme
         }}>
-        {children}
+            {children}
         </AppContext.Provider>
     )
 }
